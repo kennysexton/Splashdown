1fb78cef751ac3addfc6896ffa5cecae6e52490d
kennysexton <kennysexton4@gmail.com>
2019-08-13 10:00:17 -0400

    added favicon as well as some design changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
+    <Router exact={true}>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>Splashdown</h1>
+        <Link className="App-button" to="/generate/">
+          {/* <button className="App-button"> */}
+          Generate Color Scheme
+        {/* </button> */}
+        </Link>
       </header>
     </div>
+    <Route path="/generate" exact component={generate} />
+    </Router>
   );
 }
 
+
+function generate() {
+  return <h2>Howdy</h2>
+}
 export default App;
