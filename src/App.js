b7115491230e0615067b339167e2f7605cc26a0c
kennysexton <kennysexton4@gmail.com>
2019-08-14 14:19:31 -0400

    color swatches layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import { ScrollTo } from "react-scroll-to";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <Router exact={true}>
+    <Router>
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
@@ -19,11 +20,28 @@ function App() {
     </div>
     <Route path="/generate" exact component={generate} />
     </Router>
-  );
+  )
 }
 
 
 function generate() {
-  return <h2>Howdy</h2>
+  return (
+   
+  //  <ScrollTo>
+  //   {({ scrollTo }) => (
+  //     <a onClick={() => scrollTo({ x: 0, y: 500 })}>Scroll to Bottom</a>
+  //   )}
+    <div className="Generate-main">
+       
+          <div className="Generate-card blue"></div>
+          <div className="Generate-card mr green"></div>
+          <div className="Generate-card mr blue "></div>
+          <div className="Generate-card mr green"></div>
+          <div className="Generate-card blue"></div>
+
+    </div>
+  // </ScrollTo>
+
+  );
 }
 export default App;
