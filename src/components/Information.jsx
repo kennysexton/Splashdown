54e7116f4fe1d92fe20086b58e8bfc4ec9670bfa
kennysexton <kennysexton4@gmail.com>
2019-10-29 14:27:15 -0400

    trying to fix after url change

---
+++ src/components/Information.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export default class Information extends Component {
+    render() {
+        return (
+        <div className="Blur">sads
+
+        </div>
+        );
+    }
+}
