43c463c4ca6c03f34a6079f5e3c9b1479f2343b8
kennysexton <kennysexton4@gmail.com>
2020-03-11 14:42:19 -0400

    get rid of mixed content warning by making sure images are fetched via https

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -9,7 +9,7 @@ export default class Weather extends Component {
             // data-aos="fade-up" data-aos-duration="1000"
             <div className="weather-information">
                 <h2>{this.props.weather}</h2>
-                <img className="weather-icon" src={'http://openweathermap.org/img/wn/' + this.props.icon + '@2x.png'} />
+                <img className="weather-icon" src={'https://openweathermap.org/img/wn/' + this.props.icon + '@2x.png'} alt="current weather icon" />
             </div>
 
         );
