d848420f02758e3785dfa1ae8b1cc29d8d409e25
kennysexton <kennysexton4@gmail.com>
2021-02-09 13:51:38 -0500

    making clickable compnenets appear as pointers

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -4,7 +4,6 @@ export default class Weather extends Component {
 
     render() {
         return (
-            // data-aos="fade-up" data-aos-duration="1000"
             <div className="weather-information">
                 <h2>{this.props.weather}</h2>
                 <img className="weather-icon" src={'https://openweathermap.org/img/wn/' + this.props.icon + '@2x.png'} alt="current weather icon" />
