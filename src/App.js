51b41ded5e34377ff43c79699c63d42335b0e58b
kennysexton <kennysexton4@gmail.com>
2021-02-11 12:17:09 -0500

    spinner, function repeat call repeat fix.  more improvmenets throughout

--- src/App.js
+++ src/App.js
@@ -2,46 +2,53 @@
 // Build: npm run deploy
 
 import React, { useState, useEffect } from 'react';
+import { getRandomCity, round } from '../src/utils/common'
 
+// CSS
 import './App.css';
+import './components/spinner.css';
 import './components/settingsModal.css'
 
 //My Components
+import Spinner from './components/Spinner'
 import CityName from './components/CityName'
 import ImageBackground from './components/ImageBackground'
 import Weather from './components/Weather'
 import SettingsModal from './components/SettingsModal';
 
-// List of cities
-const citiesArray = ["Hong Kong, Hong Kong", "Singapore", "Bangkok, Thailand", "London, United Kingdom", "Paris, France", "Macau", "New York City, USA", "Shenzhen, China", "Kuala Lumpur, Malaysia", "Antalya, Turkey", "Istanbul, Turkey", "Dubai, United Arab Emirates", "Seoul, South Korea", "Rome, Italy", "Phuket, Thailand", "Guangzhou, China", "Mecca, Saudi Arabia", "Pattaya, Thailand", "Taipei, Taiwan", "Miami, USA", "Prague, Czech Republic", "Shanghai, China", "Las Vegas, USA", "Milan, Italy", "Barcelona, Spain", "Moscow, Russia", "Amsterdam, Netherlands", "Vienna, Austria", "Venice, Italy", "Los Angeles, USA", "Lima, Peru", "Tokyo, Japan", "Johannesburg, South Africa", "Beijing, China", "Sofia, Bulgaria", "Orlando, USA", "Berlin, Germany", "Budapest, Hungary", "Ho Chi Minh City, Vietnam", "Florence, Italy", "Madrid, Spain", "Warsaw, Poland", "Doha, Qatar", "Nairobi, Kenya", "Delhi, India", "Mumbai, India", "Chennai, India", "Mexico City, Mexico", "Dublin, Ireland", "San Francisco, USA", "Hangzhou, China", "Denpasar, Indonesia", "St.Petersburg, Russia", "Muğla, Turkey", "Brussels, Belgium", "Burgas, Bulgaria", "Munich, Germany", "Zhuhai, China", "Sydney, Australia", "Edirne, Turkey", "Toronto, Canada", "Lisbon, Portugal", "Cancún, Mexico", "Buenos Aires, Argentina", "Cairo, Egypt", "Punta Cana, Domincan Republic", "Suzhou, China", "Djerba, Tunisia", "Agra, India", "Kraków, Poland", "Bucharest, Romania", "Siem Reap, Cambodia", "Jaipur, India", "Honolulu, USA", "Manama, Bahrain", "Dammam, Saudi Arabia", "Hanoi, Vietnam", "Andorra La Vella, Andorra", "Nice, France", "Zürich, Switzerland", "Jakarta, Indonesia", "Manila, Philippines", "Chiang Mai, Thailand", "Marrakech, Morocco", "Sharm El Sheikh, Egypt", "Frankfurt, Germany", "Abu Dhabi, United Arab Emirates", "Vancouver, Canada", "Guilin, China", "Melbourne, Australia", "Rio De Janeiro, Brazil", "Riyadh, Saudi Arabia", "Amman, Jordan", "Sousse, Tunisia", "Kiev, Ukraine", "Sharjah, United Arab Emirates", "Jeju Island, South Korea", "Krabi, Thailand", "Artvin, Turkey"]
-
 function App() {
 
-  const firstCity = getRandomCity()
-
-  const [city] = useState(firstCity);
-  const [hidden, setHidden] = useState(false);
-  const [imgs, setImgs] = useState([]);
+  // Variables
+  const [city, setCity] = useState(null);
+  const [imgs, setImgs] = useState("");
   const [temperature, setTemperature] = useState(null);
   const [icon, setIcon] = useState(null);
-  const [loading, setLoading] = useState(false)
+
+  //Visibility
+  const [hidden, setHidden] = useState(false);
+  const [loading, setLoading] = useState(true)
 
   // ComponentDidMount() replacement
   useEffect(() => {
-    console.log('Shown City:  ' + city)
-    fetch('https://api.unsplash.com/search/photos/?page=1$per_page=1&quer=' + city + '&client_id=' + process.env.REACT_APP_UNSPLASH_KEY)
+    const randomCity = getRandomCity()
+    setCity(randomCity)
+    console.log('Shown City:  ' + randomCity)
+    const unsplashQuery = 'https://api.unsplash.com/search/photos/?page=1$per_page=1&query=' + randomCity + '&client_id=' + process.env.REACT_APP_UNSPLASH_KEY
+    fetch(unsplashQuery)
       .then(response => response.json())
       .then(data => {
         console.log("-- Image API returned")
-        console.log(data.results[0].urls)
+        console.log(data.results[0].user.links.html)
         setImgs(data.results[0].urls);
-        setLoading(false)
+        console.log()
+        setLoading(false) // TODO move this trigger to imageBackground componenet to represent to load
       })
       .catch(err => {
-        console.error(`Error happened during fetching from unsplash! ${err}`);
+        console.error(`Unsplash API error (${err})! Query: ${unsplashQuery}`);
       });
 
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&apiKey=' + process.env.REACT_APP_OPEN_WEATHER_KEY)
+    const openWeatherQuery = 'https://api.openweathermap.org/data/2.5/weather?q=' + randomCity + '&units=imperial&apiKey=' + process.env.REACT_APP_OPEN_WEATHER_KEY
+    fetch(openWeatherQuery)
       .then(response => response.json())
       .then(data => {
         console.log(`-- Weather API returned ${data.main.temp}`)
@@ -51,53 +58,34 @@ function App() {
       .catch(err => {
         console.error(`Error happened during fetching from open weather! ${err}`);
       });
-  }, [city]);
-
-
-  // Gets a random city from a predefined list
-  function getRandomCity() {
-    var length = citiesArray.length;
-    var randomNumber = Math.floor(Math.random() * length);
-    console.log(`City Selection number: ${randomNumber}`)
-    console.log(citiesArray[randomNumber])
-    return citiesArray[randomNumber]
-  }
-
-  // Roud decimal
-  function round(input) {
-    var number = Math.round(input);
-    return number;
-  }
+  }, []);
 
   function toggleHidden() {
     console.log(`inside toggle hidden`)
-    setHidden(({hidden}) => !{ hidden });
+    setHidden(({ hidden }) => !{ hidden });
   };
-
 
   return (
     <div>
       {loading ? (
-        <div>Data Loading.....</div>
+        <Spinner></Spinner>
       ) : (
-          <div>
-            <ImageBackground data={imgs} />
-
-            <div onClick={() => toggleHidden()}>
-              {/* Displays the name of the city */}
-              <CityName data={city} />
-
-              {/* Shows weather information */}
-              <Weather weather={`${round(temperature)}°F`} icon={icon}></Weather>
-            </div>
-
-            {/* Shows settings and credits */}
-            {!{ hidden } && <SettingsModal />}
-            <div className="bottomGradient"></div>
-          </div>
+          <ImageBackground data={imgs} />
         )}
-    </div>
 
+      <div onClick={() => toggleHidden()}>
+        {/* Displays the name of the city */}
+        <CityName data={city} />
+
+        {/* Shows weather information */}
+        <Weather weather={`${round(temperature)}°F`} icon={icon}></Weather>
+      </div>
+
+      {/* Shows settings and credits */}
+      {!{ hidden } && <SettingsModal />}
+      <div className="bottomGradient"></div>
+
+    </div>
   );
 }
 
