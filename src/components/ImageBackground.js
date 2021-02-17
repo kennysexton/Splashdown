a8fce2a38abb0663d2e8dd13303ad5405b124482
kennysexton <kennysexton4@gmail.com>
2021-02-17 16:16:57 -0500

    broken image work in progress

--- src/components/ImageBackground.js
+++ src/components/ImageBackground.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Spinner from './Spinner'
 
 // Uses css inline styling to take advantage of this method css centering
-const ImageBackground = props => (
-        <div style={{
-                background: 'url(' + props.data.full + ') no-repeat center center fixed',
-                backgroundSize: "cover"
-        }} className="bg">
-        </div>
-);
+function ImageBackground(props) {
+
+    const [loading] = useState(false)
+
+    return (
+        loading ? (<Spinner></Spinner>) : (
+            <div className="bg">
+                <img src={props.data.full} style={{
+                    minWidth: '100%',
+                    maxWidth: '100%',
+                    overflow: 'none',
+                    objectFit: 'contain',
+                    background: 'center center no-repeat'
+                    
+                }}>
+                </img>
+            </div >)
+
+    );
+
+}
 
 export default ImageBackground;
